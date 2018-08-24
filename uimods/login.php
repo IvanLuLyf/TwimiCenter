e8b22fbac357344a97137a7a6484b2ecc9a713f2
IvanLuLyf <835498692@qq.com>
2018-08-25 01:03:11 +0800

    update ui login

--- uimods/login.php
+++ uimods/login.php
@@ -1,29 +1,25 @@
 <?php
 if (!defined("IN_TWIMI_PHP")) die('{"status":"forbidden access"}');
-$con = TDBConnect();
-$username = GetParam("username");
-if ($username == "")
-{
-	include 'template/login.html';
-}else{
-	if ($row = TDBFetchOne("SELECT * FROM tp_user WHERE username='$username'")) {
-		if ($row['password'] == md5(GetParam("password"))) {
-			$tp_id = $row['id'];
-			$tp_token = md5(strtolower($username) . $tp_id);
-			$rsp = array('status' => 'ok', 'id' => $tp_id, 'username' => $row['username'], 'email' => $row['email'], 'token' => $tp_token, 'nickname' => $row['nickname']);
-			$updates = array('token' => $tp_token);
-			TDBUpdate($updates, "tp_user", " id='$tp_id' ");
-			session_start();  
-			$_SESSION['accesstoken'] = $tp_token;  
-			header('Location: index.php?mod=index');
-		} else {
-			$tp_error_msg = "密码错误";
-			include 'template/login.html';
-		}
-	} else {
-			$tp_error_msg = "用户名不存在";
-			include 'template/login.html';
-	}
+if (strtolower($_SERVER['REQUEST_METHOD']) == 'get') {
+    include 'template/login.html';
+} else if (isset($_POST["username"])) {
+    $username = $_POST["username"];
+    if ($row = database::getInstance()->fetchOne("select * from tp_user where username=:username", [':username' => $username])) {
+        if (isset($_POST["password"]) && $row['password'] == md5($_POST["password"])) {
+            $tp_id = $row['id'];
+            $tp_token = md5(strtolower($username) . $tp_id);
+            $rsp = ['status' => 'ok', 'id' => $tp_id, 'username' => $row['username'], 'email' => $row['email'], 'token' => $tp_token, 'nickname' => $row['nickname']];
+            $updates = ['token' => $tp_token];
+            database::getInstance()->update($updates, "tp_user", "id=:uid", [':uid' => $tp_id]);
+            session_start();
+            $_SESSION['access_token'] = $tp_token;
+            header('Location: index.php?mod=index');
+        } else {
+            $tp_error_msg = "密码错误";
+            include 'template/login.html';
+        }
+    } else {
+        $tp_error_msg = "用户名不存在";
+        include 'template/login.html';
+    }
 }
-TDBClose($con);
-?>
