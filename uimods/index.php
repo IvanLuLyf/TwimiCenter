c4be38e0ca7fea3798d795e85b34070d39dbcc60
IvanLuLyf <835498692@qq.com>
2018-08-25 00:54:52 +0800

    update ui index

--- uimods/index.php
+++ uimods/index.php
@@ -1,11 +1,8 @@
 <?php
 if (!defined("IN_TWIMI_PHP")) die('{"status":"forbidden access"}');
-$con = TDBConnect();
 $user = GetUser();
-if($user==null){
-	header('Location: index.php?mod=login');
-}else{
-	header('Location: index.php?mod=post&action=view');
+if ($user == null) {
+    header('Location: index.php?mod=login');
+} else {
+    header('Location: index.php?mod=post&action=view');
 }
-TDBClose($con);
-?>
