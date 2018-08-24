5b3a1776b67ef5e0aac936cbecb2bf9317d2b4f9
IvanLuLyf <835498692@qq.com>
2018-08-25 00:53:42 +0800

    update ajax

--- ajax.php
+++ ajax.php
@@ -1,27 +1,22 @@
 <?php
-	header("Content-Type: text/html; charset=UTF-8");
-	define("IN_TWIMI_PHP","True",TRUE);
-	define("IN_TWIMI_AJAX","True",TRUE);
-	include "config.php";
-	include "db.php";
-	function GetParam($argname){
-		$result = empty($_POST[$argname])?"":$_POST[$argname];
-		if($result == "") $result = empty($_GET[$argname])?"":$_GET[$argname];
-		return $result;
-	}
-	$mod = GetParam("mod");
-	$action = "ac_".GetParam("action");
-	include "uiauth.php";
-	if($mod!=""){
-		if(file_exists("mods/$mod.php")){
-			include "mods/$mod.php";
-			if($action!="" && is_callable($action)){
-				call_user_func($action);
-			}
-		}else{
-			echo '{"status":"mod not exists"}';
-		}
-	}else{
-		echo '{"status":"mod is empty"}';
-	}
-?>
+header("Content-Type: text/html; charset=UTF-8");
+define("IN_TWIMI_PHP", "True", TRUE);
+define("IN_TWIMI_AJAX", "True", TRUE);
+include "config.php";
+include "database.php";
+
+include "uiauth.php";
+if (isset($_REQUEST['mod'])) {
+    $mod = $_REQUEST['mod'];
+    if (file_exists("mods/$mod.php")) {
+        include "mods/$mod.php";
+        $action = isset($_REQUEST['action']) ? "ac_" . $_REQUEST['action'] : null;
+        if ($action != null && is_callable($action)) {
+            call_user_func($action);
+        }
+    } else {
+        echo '{"status":"mod not exists"}';
+    }
+} else {
+    echo '{"status":"mod is empty"}';
+}
