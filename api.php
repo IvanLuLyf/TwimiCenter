ab4619c42b9ff44e76655d2c6bab2ba06faebf89
IvanLuLyf <835498692@qq.com>
2018-08-25 00:05:54 +0800

    update api and index

--- api.php
+++ api.php
@@ -1,26 +1,21 @@
 <?php
-	header("Content-Type: text/html; charset=UTF-8");
-	define("IN_TWIMI_PHP","True",TRUE);
-	include "config.php";
-	include "db.php";
-	function GetParam($argname){
-		$result = empty($_POST[$argname])?"":mysql_escape_string($_POST[$argname]);
-		if($result == "") $result = empty($_GET[$argname])?"":mysql_escape_string($_GET[$argname]);
-		return $result;
-	}
-	$mod = GetParam("mod");
-	$action = "ac_".GetParam("action");
-	include "auth.php";
-	if($mod!=""){
-		if(file_exists("mods/$mod.php")){
-			include "mods/$mod.php";
-			if($action!="" && is_callable($action)){
-				call_user_func($action);
-			}
-		}else{
-			echo '{"status":"mod not exists"}';
-		}
-	}else{
-		echo '{"status":"mod is empty"}';
-	}
-?>
+header("Content-Type: text/html; charset=UTF-8");
+define("IN_TWIMI_PHP", "True", TRUE);
+include "config.php";
+include "database.php";
+
+include "auth.php";
+if (isset($_REQUEST['mod'])) {
+    $mod = $_REQUEST['mod'];
+    if (file_exists("mods/$mod.php")) {
+        include "mods/$mod.php";
+        $action = isset($_REQUEST['action']) ? "ac_" . $_REQUEST['action'] : null;
+        if ($action != null && is_callable($action)) {
+            call_user_func($action);
+        }
+    } else {
+        echo '{"status":"mod not exists"}';
+    }
+} else {
+    echo '{"status":"mod is empty"}';
+}
--- index.php
+++ index.php
@@ -1,28 +1,23 @@
 <?php
-	header("Content-Type: text/html; charset=UTF-8");
-	define("IN_TWIMI_PHP","True",TRUE);
-	include "config.php";
-	include "db.php";
-	function GetParam($argname){
-		$result = empty($_POST[$argname])?"":mysql_escape_string($_POST[$argname]);
-		if($result == "") $result = empty($_GET[$argname])?"":mysql_escape_string($_GET[$argname]);
-		return $result;
-	}
-	$mod = GetParam("mod");
-	$action = "ac_".GetParam("action");
-	include "uiauth.php";
-	if($mod!=""){
-		if(file_exists("uimods/$mod.php")){
-			include "uimods/$mod.php";
-			if($action!="" && is_callable($action)){
-				call_user_func($action);
-			}else if($action=="ac_" && is_callable("ac_index")){
-				call_user_func("ac_index");
-			}
-		}else{
-			echo '{"status":"mod not exists"}';
-		}
-	}else{
-		header('Location: index.php?mod=index');
-	}
-?>
+header("Content-Type: text/html; charset=UTF-8");
+define("IN_TWIMI_PHP", "True", TRUE);
+include "config.php";
+include "database.php";
+
+include "auth.php";
+if (isset($_REQUEST['mod'])) {
+    $mod = $_REQUEST['mod'];
+    if (file_exists("mods/$mod.php")) {
+        include "mods/$mod.php";
+        $action = isset($_REQUEST['action']) ? "ac_" . $_REQUEST['action'] : null;
+        if ($action != null && is_callable($action)) {
+            call_user_func($action);
+        } else if ($action == null && is_callable("ac_index")) {
+            call_user_func("ac_index");
+        }
+    } else {
+        echo '{"status":"mod not exists"}';
+    }
+} else {
+    header('Location: index.php?mod=index');
+}
