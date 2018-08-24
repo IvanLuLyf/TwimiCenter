fc63a0d728057d82ba6727539764c096763f3ba0
IvanLuLyf <835498692@qq.com>
2018-08-25 00:33:08 +0800

    update auth

--- auth.php
+++ auth.php
@@ -1,29 +1,28 @@
 <?php
 if (!defined("IN_TWIMI_PHP")) die('{"status":"forbidden access"}');
-function GetUser() {
-	$con = TDBConnect();
-	$access_token = GetParam("token");
-	$app_key = GetParam("appkey");	
-	if ($access_token == "") {
-		return null;
-	} else {
-		if ($app_key!=""){
-			if ($tokenrow = TDBFetchOne("SELECT * FROM tp_tauthtoken WHERE appkey='$app_key' and token='$access_token' and UNIX_TIMESTAMP()-expire < 0")) {
-				$uid = $tokenrow['uid'];
-				$row = TDBFetchOne("SELECT * FROM tp_user WHERE id=$uid");
-				$result = array('id' => $row['id'], 'username' => $row['username'], 'nickname' => $row['nickname'], 'email' => $row['email']);
-				return $result;
-			} else {
-				return null;
-			}
-		}else{
-			if ($row = TDBFetchOne("SELECT * FROM tp_user WHERE token='$access_token'")) {
-				$result = array('id' => $row['id'], 'username' => $row['username'], 'nickname' => $row['nickname'], 'email' => $row['email']);
-				return $result;
-			} else {
-				return null;
-			}
-		}
-	}
+function GetUser()
+{
+    if (!isset($_POST['token'])) {
+        return null;
+    } else {
+        $access_token = $_POST['token'];
+        if (isset($_POST['appkey'])) {
+            $app_key = $_POST['appkey'];
+            if ($token_row = database::getInstance()->fetchOne("select * from tp_tauthtoken where appkey=:ak and token=:tk and UNIX_TIMESTAMP()-expire < 0", [':ak' => $app_key, ':tk' => $access_token])) {
+                $uid = $token_row['uid'];
+                $user_row = database::getInstance()->fetchOne("select * from tp_user where id=:uid", [':uid' => $uid]);
+                $result = array('id' => $user_row['id'], 'username' => $user_row['username'], 'nickname' => $user_row['nickname'], 'email' => $user_row['email']);
+                return $result;
+            } else {
+                return null;
+            }
+        } else {
+            if ($user_row = database::getInstance()->fetchOne("select * from tp_user where token=:tk", [':tk' => $access_token])) {
+                $result = array('id' => $user_row['id'], 'username' => $user_row['username'], 'nickname' => $user_row['nickname'], 'email' => $user_row['email']);
+                return $result;
+            } else {
+                return null;
+            }
+        }
+    }
 }
-?>
