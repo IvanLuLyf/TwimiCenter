b7eaccc1eda9cff49d1679a05bae640870807547
IvanLuLyf <835498692@qq.com>
2018-08-26 17:20:37 +0800

    update config format

--- config.php
+++ config.php
@@ -1,11 +1,10 @@
 <?php
-	if(!defined("IN_TWIMI_PHP"))die('{"status":"forbidden access"}');
-	date_default_timezone_set('PRC'); 
-	define("DB_HOST","127.2.194.130");
-	define("DB_NAME","twimi");
-	define("DB_USER","adminlwestpL");
-	define("DB_PASS","w7IPg5U5viwz");
-	
-	define("TP_SITENAME","TwimiPHP");
-	define("TP_SITEURL","tp.twimi.cn");
-?>
+if (!defined("IN_TWIMI_PHP")) die('{"status":"forbidden access"}');
+date_default_timezone_set('PRC');
+define("DB_HOST", "localhost");
+define("DB_NAME", "database");
+define("DB_USER", "username");
+define("DB_PASS", "password");
+
+define("TP_SITE_NAME", "Your Site Name");
+define("TP_SITE_URL", "example.com");
