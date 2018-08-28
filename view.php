f5f54af13241ed6eac0927415a0144a9d7d428dc
IvanLuLyf <835498692@qq.com>
2018-08-28 17:51:34 +0800

    add View.php

---
+++ view.php
@@ -0,0 +1,30 @@
+<?php
+
+class view
+{
+    public static function get_url($mod, $action, $params = [])
+    {
+        $query = http_build_query($params);
+        return "index.php?mod=$mod&action=$action&$query";
+    }
+
+    public static function redirect($url, $action = null, $params = [])
+    {
+        if ($action == null) {
+            header("Location: $url");
+        } else {
+            $query = http_build_query($params);
+            header("Location: index.php?mod=$url&action=$action&$query");
+        }
+    }
+
+    public static function render($view, $context = [])
+    {
+        if (defined("IN_TWIMI_API") or defined("IN_TWIMI_AJAX")) {
+            echo json_encode($context);
+        } else {
+            extract($context);
+            include "template/$view";
+        }
+    }
+}
