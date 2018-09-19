28683fa15cd2f655f43629b29c638c3f8d1893fe
IvanLuLyf <835498692@qq.com>
2018-09-19 16:25:52 +0800

    Add Register to UserService

--- service/UserService.php
+++ service/UserService.php
@@ -25,4 +25,29 @@ class UserService
             return ['ret' => 1002, 'status' => "user not exists", 'tp_error_msg' => "用户名不存在"];
         }
     }
+
+    public function register($username, $password, $email, $nickname = '')
+    {
+        if (isset($password) && isset($email)) {
+            if (preg_match('/^[A-Za-z0-9_]+$/u', $username) && strlen($username) >= 4) {
+                if ($row = database::getInstance()->fetchOne("select * from tp_user where username=:username", ['username' => $username])) {
+                    return ['ret' => 1003, 'status' => "user exists", 'tp_error_msg' => "用户名已存在"];
+                } else {
+                    $nickname = $nickname == '' ? $username : $nickname;
+                    $timestamp = time();
+                    $tp_token = md5($password . strtolower($username) . $timestamp);
+                    $user_data = ['username' => $username, 'email' => $email, 'password' => md5($password), 'nickname' => $nickname, 'token' => $tp_token, 'expire' => $timestamp + 604800];
+                    if ($uid = database::getInstance()->insert($user_data, 'tp_user')) {
+                        return ['ret' => 0, 'status' => 'ok', 'id' => $uid, 'username' => $username, 'email' => $email, 'token' => $tp_token, 'nickname' => $nickname];
+                    } else {
+                        return ['ret' => 1006, 'status' => "database error", 'tp_error_msg' => "数据库内部出错"];
+                    }
+                }
+            } else {
+                return ['ret' => 1005, 'status' => "invalid username", 'tp_error_msg' => "用户名仅能为字母数字且长度大于4"];
+            }
+        } else {
+            return ['ret' => 1004, 'status' => "empty arguments", 'tp_error_msg' => "参数不能为空"];
+        }
+    }
 }
